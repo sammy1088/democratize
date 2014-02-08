97b7c68bf6287ec47f2797c4e3d0075ab8cc50b6
Sammy1088 <szshreder@gmail.com>
2014-02-08 12:27:58 -0600

    db probz

--- db/migrate/20140126034315_change_votes_polymorphic.rb
+++ db/migrate/20140126034315_change_votes_polymorphic.rb
@@ -1,6 +1,6 @@
 class ChangeVotesPolymorphic < ActiveRecord::Migration
   def change
-  	remove_column :votes, :comment_id
+  	
   	remove_column :votes, :link_id
   	add_column :votes, :votable_id, :integer
   	add_column :votes, :votable_type, :integer
