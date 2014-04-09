e5a1f2d7aae368fb7efc450d3048824fd23a156a
Sammy1088 <szshreder@gmail.com>
2014-04-09 20:43:39 +0000

    -falserake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,6 @@ Sawit::Application.configure do
   }
 }
 
-  SitemapGenerator.verbose = falserake
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
