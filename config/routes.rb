7f23c1c7a4ba136f7512e3811ba0d63f72cd9667
Sammy1088 <szshreder@gmail.com>
2014-05-10 17:02:25 +0000

    slkdfj

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ Sawit::Application.routes.draw do
 
   
 
-  root :to => "cities#show", :id => '1'
+  root :to => "states#show", :id => '2'
 
   resources :states   do
     resources :links
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,17 +3,11 @@ SitemapGenerator::Sitemap.default_host = "http://www.democratize.io"
 
 SitemapGenerator::Sitemap.create do
   
-  add '/cities'
-  add '/states'
+  add '/cities/1'
+  add '/cities/2'
+  add '/states/2'
   
-  add cities_path, :priority => 0.7, :changefreq => 'daily'
-  City.find_each do |city|
- add city_path(city), :lastmod => city.updated_at
-end
-    add states_path, :priority => 0.7, :changefreq => 'daily'
-  State.find_each do |state|
-    add state_path(state), :lastmod => state.updated_at
-end
+
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
