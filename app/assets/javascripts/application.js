3d4fb1ff5a788150a812ce7c350f6f8d736d4cae
Sammy1088 <szshreder@gmail.com>
2014-05-22 16:26:13 +0000

    sdklfj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,18 +25,41 @@ $(".reply-link").click(function(e){
     $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+$('.sdf').on('click', function(){
+  $(this).html('<b>Supported!</b>');
+  $(this).next().html('<em>+1</em>');
+})
+
+$('.sfd').on('click', function(){
+  $(this).html('<b>Opposed!</b>');
+  $(this).next().html('<em>-1</em>');
+})
+
+$('.btrigger').on('click', function(){
+  $(this).html('<div class="glyphup"><span class="glyphicon glyphicon-chevron-up"></span></div>');
+  $(this).parent().next().html('<em>+1</em>');
+})
+
 $('.trigger').on('click', function(){
   $(this).html('<div class="glyphup"><span class="glyphicon glyphicon-chevron-up"></span></div>');
   $(this).parent().next().html('<em>+1</em>');
 })
-  
+
+$('.ctrigger').on('click', function(){
+  $(this).html('<div class="ctriggerJ"><span class="glyphicon glyphicon-chevron-up"></i></div>');
+})
+ 
+$('.ctrigger-').on('click', function(){
+  $(this).html('<div class="ctriggerJx"><span class="glyphicon glyphicon-chevron-down"></i></div>');
+})
+
 $('.trigger-').on('click', function(){
   $(this).html('<div class="glyphdown"><span class="glyphicon glyphicon-chevron-down"></span></div>');
   $(this).parents('.votes').find("#votesS").html('<em>-1</em>');
 })
 
 $('.actions').click(function(){
-  $(this).html('<b>comment successfully submited!</b>');
+  $(this).html('<b>comment submited!</b>');
   $(this).submit();
 })
   
